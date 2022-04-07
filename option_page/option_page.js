800c786284598abd04f4439eea2086287051081b
Mampionona33 <ramamps33@gmail.com>
2022-04-07 12:43:08 +0300

    finish to put value in local storage

--- option_page/option_page.js
+++ option_page/option_page.js
@@ -1,8 +1,27 @@
-const registerInputsValue = () => {
+const registerInputsValue = async () => {
+  // get all value and id of inputs in form
   const allInputValue = Array.from(
     document.querySelectorAll('#replaceformId input')
   ).reduce((acc, input) => ({ ...acc, [input.id]: input.value }), {});
-  console.log(allInputValue);
+
+  const values = [];
+  const keys = [];
+
+  //   Get array values
+  for (let value of Object.values(allInputValue)) {
+    values.push(value);
+  }
+  //   Get array keys
+  for (let key of Object.keys(allInputValue)) {
+    keys.push(key);
+  }
+
+  for (let i = 0; i < keys.length; i++) {
+    //   if value is not empty put the key and the value in the session storage
+    if (values[i] !== '') {
+      sessionStorage.setItem(keys[i], values[i]);
+    }
+  }
 };
 
 document.getElementById('btnSubmit').addEventListener('click', (event) => {
